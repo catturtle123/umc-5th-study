8350a28fc995fffdf4442b466e91cd9cc914e542
jamey <musoyou1085@gmail.com>
2023-11-13 21:30:41 +0900

    mission8

---
+++ src/main/java/umc/spring/service/TempCommandQueryImpl.java
@@ -0,0 +1,17 @@
+package umc.spring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.spring.base.Code;
+import umc.spring.base.exception.handler.TempHandler;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandQueryImpl implements TempQueryService{
+
+    @Override
+    public void CheckFlag(Integer flag) {
+        if (flag == 1)
+            throw new TempHandler(Code.TEMP_EXCEPTION);
+    }
+}
